ff89b162d2d0e06346616c098b70c5e7bceaa5b2
Hidravneno <francisco.arambuloreyes@cesunbc.edu.mx>
2024-08-02 14:48:48 -0700

    ACOMDOO DE TODO EL CODIGO

--- scripttt.js
+++ scripttt.js
@@ -1,11 +1,17 @@
 window.onload = () => {
-    console.time('Carga de la página');
-    
-    document.addEventListener('DOMContentLoaded', () => {
-        console.timeEnd('Carga de la página');
-    });
+  console.time("Carga de la página");
 
-    // Verificación de elementos
-    console.assert(document.querySelector('.nav-bar') !== null, 'Error: Navbar no cargada');
-    console.assert(document.querySelector('.card-container') !== null, 'Error: Tarjetas de productos no cargadas');
+  document.addEventListener("DOMContentLoaded", () => {
+    console.timeEnd("Carga de la página");
+  });
+
+  // Verificación de elementos
+  console.assert(
+    document.querySelector(".nav-bar") !== null,
+    "Error: Navbar no cargada"
+  );
+  console.assert(
+    document.querySelector(".card-container") !== null,
+    "Error: Tarjetas de productos no cargadas"
+  );
 };
